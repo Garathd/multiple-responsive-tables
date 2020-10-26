d6fd75354e3f3c67e8daab14255a0293783427f7
Garathd <garath.davis.gti@gmail.com>
2020-10-26 16:44:29 +0000

    Changed Plugin Information

--- responsive-table.php
+++ responsive-table.php
@@ -1,9 +1,11 @@
 <?php
 /*
-Plugin Name: Mulitple Responsive Tables
+Plugin Name: GDX Responsive Tables
+Plugin URI: https://www.gdxdesigns.com/plugins/responsive-tables
 Description: Having Multiple Responsive Tables
-Author: Garath Davis
+Author: GDX Designs
 Version: 1.0
+Author URI: https://www.gdxdesigns.com/
 */
 
 define('MAIN_DIR', plugin_dir_path(__FILE__));
