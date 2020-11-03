15922a1f022d84b466304b9e5564cdfc3ea0db9c
Garathd <garath.davis.gti@gmail.com>
2020-11-03 17:14:56 +0000

    updated the plugin url

--- responsive-table.php
+++ responsive-table.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: GDX Responsive Tables
-Plugin URI: https://www.gdxdesigns.com/plugins/responsive-tables
+Plugin URI: https://gdxdesigns.com/plugins/responsive-tables
 Description: Having Multiple Responsive Tables
 Author: GDX Designs
 Version: 1.0
-Author URI: https://www.gdxdesigns.com/
+Author URI: https://gdxdesigns.com/
 */
 
 define('GDX_RESPONSIVE_TABLE_DIR', plugin_dir_path(__FILE__));
